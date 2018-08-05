f3759eae3cf89f614acef352f25e9165adc05db8
lorenareyes <lorenacrisreyes@gmail.com>
2018-08-05 03:10:08 -0500

    geolocalizacion, inicio de places

---
+++ map.js
@@ -0,0 +1,80 @@
+var message = document.getElementById("message");
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(initAutocomplete);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+//iniciando la funcion del callback de la url
+function initAutocomplete(position) {
+    const mapElement = document.getElementById('map')
+    const objMap = {
+    center: {lat: position.coords.latitude, lng: position.coords.longitude},
+    zoom: 13,
+    mapTypeId: 'roadmap'
+}
+    var map = new google.maps.Map(mapElement, objMap);
+
+    // Crea el cuadro de búsqueda y vincúlalo al elemento UI.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Parcializa los resultados de SearchBox hacia la vista del mapa actual.
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+// Escucha el evento disparado cuando el usuario selecciona una predicción y recupera
+    // más detalles para ese lugar
+    searchBox.addListener('places_changed', function() {
+      var places = searchBox.getPlaces();
+
+      if (places.length == 0) {
+        return;
+      }
+
+     
+// Borrar los marcadores anteriores.
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      markers = [];
+
+
+// Para cada lugar, obtenga el icono, el nombre y la ubicación.
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        // Crea un marcador para cada lugar.
+        markers.push(new google.maps.Marker({
+          map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
+
+        if (place.geometry.viewport) {
+          // Solo los geocódigos tienen una ventana gráfica.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+  }
